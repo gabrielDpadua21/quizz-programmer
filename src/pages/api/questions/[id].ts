07732cccec7a56ecfe371795294df2f44a92509d
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-04-04 01:02:27 -0300

    feat: api setup

---
+++ src/pages/api/questions/[id].ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    response.status(200).json({
+        message: "Hello World"
+    })
+}
