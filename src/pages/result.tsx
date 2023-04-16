ae19a45332159bfdcbd3da21c0cb84761b8303fd
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-04-16 03:29:28 -0300

    feat: result component

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,9 +1,25 @@
-
+import styles from "../styles/Result.module.css";
+import { useRouter } from "next/router";
 
 const Result: React.FC = () => {
+    const router = useRouter();
+
+    const total = +router.query.total | 0;
+    const correct = +router.query.correct | 0;
+    const percent = Math.round((correct / total) * 100);
+    
     return (
-        <div>
-            <h1>Result</h1>
+        <div className={styles.result}>
+            <h1>Final Result</h1>
+            <span>
+                {total}
+            </span>
+            <span>
+                {correct}
+            </span>
+            <span>
+                {percent}%
+            </span>
         </div>
     );
 }
